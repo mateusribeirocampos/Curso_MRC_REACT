38f786377a0894590c7b3dd82c3cfbba61ecef55
Mateus Campos <mateusrcampos@gmail.com>
2022-12-06 14:59:55 -0300

    First step and fundamentos com componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import FirstComponents from './componentes/FirstComponents';
+
+//Style
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fundamentos para aplicação de componentes no React</h1>
+      <p>Quais são as funções utilizadas para adicionar componentes</p>
+      <FirstComponents/>
     </div>
   );
 }
---
+++ src/componentes/FirstComponents.js
@@ -0,0 +1,7 @@
+const FirstComponents = () => {
+    return (
+        <h1>Texto adicionado nos componentes para utilização no App.js</h1>
+    );
+};
+
+export default FirstComponents; 
