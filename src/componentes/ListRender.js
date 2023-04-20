6cd99aa46547eb13190364f66e310a56ccc66a9d
mateusribeirocampos <mateusrcampos@gmail.com>
2023-04-20 09:54:52 -0300

    Deletar randomicamente usuários

--- src/componentes/ListRender.js
+++ src/componentes/ListRender.js
@@ -3,11 +3,19 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState (["Rodrigo", "Thiago", "Pedro", "Mateus", "Eduardo"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name: "João", age: 32},
         {id: 2, name: "Mateus", age: 42},
         {id: 3, name: "Laura", age: 39},
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers ((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+    };
 
   return (
     <div>
@@ -22,6 +30,7 @@ const ListRender = () => {
             {users.map((user) => 
             <li key={user.id}>{user.name} - {user.age}</li>)}
         </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
