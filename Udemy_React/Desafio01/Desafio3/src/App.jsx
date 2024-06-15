34607c2c46db7f0a0dea68c2867fcaa52f069e48
mateusribeirocampos <mateusrcampos@gmail.com>
2024-06-15 18:25:09 -0300

    desafio1 concluído

---
+++ Udemy_React/Desafio01/Desafio3/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+       <h1>Vamos Avançar no REACT</h1>
+       <p>Iniciar seção 3 </p>
+      </div>
+    </>
+  )
+}
+export default App
